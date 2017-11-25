9dc178f1c240e22adde04ab863f3763dbecf31ef
ImmanuelBaskaran <immanuel.baskaran.16@ucl.ac.uk>
2017-11-25 20:04:00 +0000

    Add TABLE and misc

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,14 +4,18 @@ import { Container } from "reactbulma";
 import TextEditor from "../../components/TextEditor";
 import Options from "../../components/Options";
 import Cells from "./Cells";
+import Example from "../../components/DataGrid";
 
 export default class Home extends Component {
 
     render() {
         return (
             <Container is-fluid>
+
                 <Options />
+                <TextEditor/>
                 <Cells />
+                <Example />
             </Container>
         );
     }
